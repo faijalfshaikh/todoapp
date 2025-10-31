1043bef0ee283e8ff237023247bae201e80bf5a1
=faijalfshaikh <=faijalfshaikh@gmail.com>
2025-10-31 16:48:53 +0530

    Added Toggle Theme Function

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 import React from 'react'
+import useTheme from '@/hooks/useTheme'
 
 const index = () => {
+
+    const {toggleDarkMode} = useTheme();
+
   return (
-    <View>
-      <Text>index</Text>
+    <View style={styles.container}>
+      <Text style={styles.content}>This is the index page</Text>
+      <Text>hi</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Toggle the Theme</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap:10
+    },
+    content:{
+        fontSize:20
+    }
+})
 
 export default index
